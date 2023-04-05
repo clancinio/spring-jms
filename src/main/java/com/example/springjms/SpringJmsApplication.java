a854ffa0c1f668dc1c7f6a4d08ac972ad862bc1a
clancinio <dean.clancy91@gmail.com>
2023-04-05 10:59:42 +0100

    Init commit

---
+++ src/main/java/com/example/springjms/SpringJmsApplication.java
@@ -0,0 +1,15 @@
+package com.example.springjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+
+@EnableJms
+@SpringBootApplication
+public class SpringJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJmsApplication.class, args);
+	}
+
+}
